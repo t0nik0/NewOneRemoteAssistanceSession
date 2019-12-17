17f8c2569757dc33f2ddc27e7fdffebc7ea31358
t0nik0 <dantonov@gmail.com>
2019-12-17 20:09:46 +0300

    Add files via upload

---
+++ NewOneRemoteAssistanceSession.ps1
@@ -0,0 +1,37 @@
+# It run a remote assistance session with "random" pass
+# and send it and base64 message of the invitation file
+# to my ifttt's webhook and copy itself to %desktop% )
+# It was wrotten for my parents.
+
+$myiftttevent = "MyIFTTTEventName"
+$myiftttkey = "My_Shiny_New_Long-Long_IFTTT_Key"
+
+Copy-Item -Path $PSCommandPath -Destination $([Environment]::GetFolderPath("Desktop"))
+
+$pass = ( -join ((0x30..0x39) + ( 0x41..0x5A) | Get-Random -Count 12  | % {[char]$_}) )
+$file = (Join-Path -Path $Env:temp -ChildPath "Invitation.msrcIncident")
+$zip = (Join-Path -Path $Env:temp -ChildPath "I.zip")
+Remove-Item $file 2> $null
+Remove-Item $zip 2> $null 
+
+msra /saveasfile $file $pass
+Start-Sleep -Seconds 3
+
+$file = Get-Item $file
+Compress-Archive -Path $file -DestinationPath $zip
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($zip))
+Remove-Item $file 2> $null
+Remove-Item $zip 2> $null 
+
+$webhookUrl = "https://maker.ifttt.com/trigger/$myiftttevent/with/key/$myiftttkey"
+$body = @{
+        value1 = $pass
+        value2 = $base64string
+        value3 = $Env:username
+    }
+
+Invoke-RestMethod -Method Get -Uri $webhookUrl -Body $body
+
+# just for debugging
+#Write-Host -NoNewLine 'Press any key to continue...';
+#$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
